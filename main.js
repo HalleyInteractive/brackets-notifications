8af80b73a9a613d8dafea32c3b84487aa6ec59f4
Niels Overwijn <niels.overwijn@gmail.com>
2014-05-20 21:43:06 +0200

    Removed the Node part for now to simplefy development

--- main.js
+++ main.js
@@ -7,41 +7,25 @@ define(function (require, exports, module)
 	
 	// Load brackets modules
 	var ExtensionUtils = brackets.getModule("utils/ExtensionUtils"),
-        NodeDomain     = brackets.getModule("utils/NodeDomain");
-
-	// Load node domain
-    var NotificationsDomain = new NodeDomain("notifications", ExtensionUtils.getModulePath(module, "node/Notifications"));
+		CommandManager = brackets.getModule("command/CommandManager");
 
 	// Load stylesheet
 	ExtensionUtils.loadStyleSheet(module, "brackets-notifications.css");
 
 	// All notifications are appended to this container
 	$(".main-view .content #editor-holder").append("<div id='notifications-container'></div>");
-
-	// Simple notification event listener
-	$(NotificationsDomain).on("simple-notification", function(event, parameters)
-	{
-		console.group("Simple Notification Received");
-		console.log(event);
-		console.log(parameters);
-		console.log("Notification received - title: " + parameters.title);
-		console.log("Notification received - message: " + parameters.message);
-		console.groupEnd();
-
-		constructSimpleNotification(parameters.title, parameters.message);
-	});
 	
 	// Adds the notification to the notifications container
 	function constructSimpleNotification(title, message)
 	{
+		console.log("Construct Simple Notification");
 		var simpleNotification = $("<div class='simple-notification'><h3>"+title+"</h3><p>"+message+"</p></div>");
 		$("#notifications-container").append(simpleNotification);
 		simpleNotification.delay(2000).fadeOut();
 	}
 
-	// Sample of triggering a notification
-	NotificationsDomain.exec("simple-notification", "Sample title", "Sample message")
-	.done(function(msg) { console.log("simple-notification triggered: " + msg); })
-	.fail(function(msg) { console.log("simple-notification triggering failed: " + msg); });
+    // First, register a command - a UI-less object associating an id to a handler
+    var SIMPLE_NOTIFICATION_COMMAND_ID = "notifications.simple";   // package-style naming to avoid collisions
+    CommandManager.register("Send Simple Notification notification", SIMPLE_NOTIFICATION_COMMAND_ID, constructSimpleNotification);
 
 });
